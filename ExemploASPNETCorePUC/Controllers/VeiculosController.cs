7b1d8aec5d5584cc107038f143493b3801b9f322
Geisson Oliveira <geissonlucaso@gmail.com>
2024-10-09 12:17:48 -0300

    Criação da Controller e View Index de Veículos.

---
+++ ExemploASPNETCorePUC/Controllers/VeiculosController.cs
@@ -0,0 +1,23 @@
+﻿using ExemploASPNETCorePUC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExemploASPNETCorePUC.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
--- ExemploASPNETCorePUC/Models/Veiculo.cs
+++ ExemploASPNETCorePUC/Models/Veiculo.cs
@@ -17,9 +17,11 @@ namespace ExemploASPNETCorePUC.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
     }
