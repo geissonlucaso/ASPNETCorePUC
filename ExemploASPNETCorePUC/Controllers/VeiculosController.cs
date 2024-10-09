01abcdb0ad88c1ea83c7c32e5828aaab390202fb
Geisson Oliveira <geissonlucaso@gmail.com>
2024-10-09 15:42:19 -0300

    Implementação da edição de veículos.

--- ExemploASPNETCorePUC/Controllers/VeiculosController.cs
+++ ExemploASPNETCorePUC/Controllers/VeiculosController.cs
@@ -40,5 +40,33 @@ namespace ExemploASPNETCorePUC.Controllers
             return View(veiculo);
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+                return NotFound();
+
+            return View(veiculo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
